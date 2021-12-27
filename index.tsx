963bd7c733e2ff1d2815ea5f4e8685f6e7089a85
OdedShimoni <odedspanki@gmail.com>
2021-12-27 19:00:34 +0200

    Initial commit

---
+++ index.tsx
@@ -0,0 +1,16 @@
+import React, { useState } from 'react';
+import { render } from 'react-dom';
+import Hello from './Hello';
+import './style.css';
+
+function App() {
+  const [name, setName] = useState('Oded');
+  return (
+    <div>
+      <Hello {...{name}} />
+      <p>Start editing to see some magic happen :) Started editing</p>
+    </div>
+  );
+}
+
+render(<App />, document.getElementById('root'));
